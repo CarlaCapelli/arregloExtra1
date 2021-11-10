e7bef5057d82c10198db89b7e9648b84534d9f0b
CarlaCapelli <92063946+CarlaCapelli@users.noreply.github.com>
2021-11-10 09:00:46 -0300

    agregaParametros

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
-function miFuncion() {
-  let cant: number = Number(prompt("cantidad de elementos"));
-  let valor = String(prompt("valor del arreglo"));
-
+function miFuncion(cant: number, valor: string): any {
   let arreglo: string[] = new Array(cant);
   for (let i: number = 0; i < cant; i++) {
     arreglo[i] = valor;
-    console.log(arreglo[i]);
+    console.log(arreglo);
   }
 }
-miFuncion();
+miFuncion(3, "a");
